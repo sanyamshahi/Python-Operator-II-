0372ec11a06991a1681ea4f688a0181423b8ff21
sanyamshahi <ersatishshahi@gmail.com>
2026-02-21 20:28:26 +0545

    Add files via upload

---
+++ age.py
@@ -0,0 +1,6 @@
+age=int(input("enter your age"  ))
+nationalality=input("are you an indian citizen (yes/no)  ")
+if age>=18 or nationalality=="yes":
+    print("you are eligible to vote")
+else:
+    print("you are not eligibe to vote")
